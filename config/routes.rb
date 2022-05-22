a33a6c2bccc52536585bc187da5e0680ec1ff63a
Mahmoud Saudi <m_m_kassem@hotmail.com>
2022-05-22 17:31:32 +0200

    added API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json}  do
+    namespace :v1 do
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+      resources :applications, param: :token do
+        member do
+          resources :chats, param: :chat_number do
+            member do
+              resources :messages, param: :message_number
+            end
+          end
+        end
+      end
+    end
+  end
 end
